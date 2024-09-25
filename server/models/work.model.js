9102ed57c17b166e7ca8347ae0fb7c8bc72a96cb
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-09-25 18:47:27 +0530

    fix:typo's due to merge

--- server/models/work.model.js
+++ server/models/work.model.js
@@ -1,49 +1,47 @@
-import { Schema, model } from 'mongoose'
+import mongoose from 'mongoose'
 
-const workSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        pay: {
-            type: Number,
-            required: true,
-        },
-        joblevel: {
-            type: String,
-            enum: {
-                values: ['Easy','Medium','Hard'],
-                message: '{VALUE} is invalid',
-            },
-            default: 'Medium',
-        },
-        skills_Required: {
-            type: [String],
-            required: true,
-        },
-        applied_status: {
-            type: [String]
-        },
-        active: {
-            type: Boolean,
-            default: true,
-        },
-        client_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-        freelancer_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-    }   
-)
+const workSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  pay: {
+    type: Number,
+    required: true,
+  },
+  joblevel: {
+    type: String,
+    enum: {
+      values: ['Easy', 'Medium', 'Hard'],
+      message: '{VALUE} is invalid',
+    },
+    default: 'Medium',
+  },
+  skills_Required: {
+    type: [String],
+    required: true,
+  },
+  applied_status: {
+    type: [String],
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  client_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  freelancer_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+})
 
-const Work = model('Work', workSchema)
+const Work = mongoose.model('Work', workSchema)
 
 export default Work
