1d7ea75e0cdadf2dfc7cc24d8089d4b765fd7827
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-12-02 19:18:12 +0530

    fix:make sidebar sticky

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { SideBarElement } from './SideBarElement';
 
 const Sidebar = () => {
   return (
-    <div className="hidden h-screen w-80 bg-gray-900 px-5 py-16 text-xl text-gray-200 sm:block">
+    <div className="sticky top-0 hidden h-screen w-80 bg-gray-900 px-5 py-16 text-xl text-gray-200 sm:block">
       <p className="mb-10 rounded-[40px] border border-teal-400 bg-teal-900 py-2 text-center text-3xl font-medium">
         <span className="font-semibold text-teal-300">WorkSphere</span>.
       </p>
