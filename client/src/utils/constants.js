a780bfcb8d8c7a1e0d59e8fe879c46a2834a2732
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2025-05-06 21:49:52 +0530

    chore: Update API Endpoints

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://worksphere-dq95.onrender.com/api/v1';
-export const IMAGE_URL = 'https://worksphere-dq95.onrender.com/';
-//
-// export const API_URL = 'http://localhost:3000/api/v1';
-// export const IMAGE_URL = `http://localhost:3000/`;
+// export const API_URL = 'https://worksphere-dq95.onrender.com/api/v1';
+// export const IMAGE_URL = 'https://worksphere-dq95.onrender.com/';
+
+export const API_URL = 'https://worksphere-t0zyh.kinsta.app/api/v1';
+export const IMAGE_URL = 'https://worksphere-t0zyh.kinsta.app/';
